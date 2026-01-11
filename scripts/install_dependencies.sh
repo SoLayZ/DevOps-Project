9da525521d5a49122f57f4a6abfec46f3eaa5c0d
EC2 Default User <ec2-user@ip-172-31-32-59.ap-southeast-1.compute.internal>
2026-01-11 17:45:04 +0000

    fixed proxy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,8 +8,8 @@ sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
   DefaultType text/html
   ProxyRequests off
   ProxyPreserveHost On
-  ProxyPass / http://localhost:8080/nextwork-web-project/
-  ProxyPassReverse / http://localhost:8080/nextwork-web-project/
+  ProxyPass / http://localhost:8080/devops-project/
+  ProxyPassReverse / http://localhost:8080/devops-project/
 </VirtualHost>
 EOF
 
